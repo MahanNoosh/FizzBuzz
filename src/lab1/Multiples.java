ad6fabd2f0ee5158df2c650670fa3503debe21cf
MahanNoosh <mahan.mnnr@gmail.com>
2025-09-09 19:43:49 -0400

    ready to submit

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class Multiples {
+    static void main(String[] args) {
+        int ans = multiples(1000, 3, 5);
+        System.out.println(ans);
+    }
+
+    static int multiples(int n, int a, int b) {
+        int ans = 0;
+        for (int i = 1; i < n; i++){
+            if (i % a == 0 || i % b == 0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
